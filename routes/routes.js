dc9d82346c51eb26c2fb42e5f64d00fce6d2a155
Matt <mbatko0914@gmail.com>
2020-06-25 22:02:59 -0400

    updated npm packages & added html routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,20 @@
+const path = require('path');
+const db = require('../models');
 
 // html routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 
+app.get('/exercise', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/exercise.html'));
+});
 
+app.get('stats/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/stats.html'));
+});
 
 // api routes
-
 app.get("/api/stats", (req, res) => {
   db.User.find({})
     .then(dbUser => {
